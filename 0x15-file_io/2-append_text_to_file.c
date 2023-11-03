c1f1d72eee11efcd9b0cc8f409a0f4d02e56c295
thabobrian <thabodhlomo05@gmail.com>
2023-11-03 12:59:25 -0700

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,8 +6,8 @@
 
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
+ * @filename: the  filename
+ * @text_content: added content
  *
  * Return: 1 on success and -1 on failure
  */
